b4d8dbd197069847aebc8f54da4794b9581cb4dd
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2023-09-08 14:57:25 +0900

    readmeを修正

--- app/main.py
+++ app/main.py
@@ -7,15 +7,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+#フォームに入力された数値を倍にする処理
 @app.route('/multiply', methods=['POST'])
 def multiply():
+    #フォームから数値を受け取る
     number = int(request.form['number'])
+    #数値が負の数なら失敗にする
     if number<0:
         return render_template('result-failure.html',result=number)
+    #正の数なら倍にする処理を実行して返す
     else:
         result = number * 2
         return render_template('result-success.html', result=result)
 
+#テスト用のページ
 @app.route("/test")
 def test():
     return render_template('test.html')
