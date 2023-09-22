2701806b5071e054afa602087b4b79610bab1a5f
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2023-09-22 10:47:38 +0900

    テーブル内容を修正

--- db/sql/init.sql
+++ db/sql/init.sql
@@ -1,29 +1,28 @@
 
 # テーブルemployeeの作成
 create table employee(
-	ident	int auto_increment primary key,
+	number	int auto_increment primary key,
 	name	varchar(50) not null,
 	category	varchar(50) not null,
 	auth	int not null,
-	number	int,
 	gender	varchar(20),
 	start	int not null,
 	finish	int not null,
 	pass	varchar(3000)
-);
+) auto_increment = 123200;
 
 # テーブルemployeeへデータを入力
-insert into employee(name, category, auth, number, gender, start, finish, pass)
-	values('嫁阪雄大', '店長', 0, 123209, 'male', 18, 21, 'None');
-insert into employee(name, category, auth, number, gender, start, finish, pass)
-	values('木下秀吉', '副店長', 1, 123229, 'male', 8, 17, 'None');
-insert into employee(name, category, auth, number, gender, start, finish, pass)
-	values('飯野美沙', '副店長', 1, 100000, 'female', 12, 21, 'None');
-insert into employee(name, category, auth, number, gender, start, finish, pass)
-	values('富士富雄', '管理', 2, 000022, 'male', 12, 21, 'None');
-insert into employee(name, category, auth, number, gender, start, finish, pass)
-	values('大空スバル', 'レジ統合', 2, 121212, 'female', 17, 21, 'None');
-insert into employee(name, category, auth, number, gender, start, finish, pass)
-	values('岸辺露伴', '書籍', 2, 123210, 'male', 8, 15, 'None');
+insert into employee(name, category, auth, gender, start, finish, pass)
+	values('嫁阪雄大', '店長', 0, 'male', 18, 21, 'None');
+insert into employee(name, category, auth, gender, start, finish, pass)
+	values('木下秀吉', '副店長', 1, 'male', 8, 17, 'None');
+insert into employee(name, category, auth, gender, start, finish, pass)
+	values('飯野美沙', '副店長', 1, 'female', 12, 21, 'None');
+insert into employee(name, category, auth, gender, start, finish, pass)
+	values('富士富雄', '管理', 2, 'male', 12, 21, 'None');
+insert into employee(name, category, auth, gender, start, finish, pass)
+	values('大空スバル', 'レジ統合', 2, 'female', 17, 21, 'None');
+insert into employee(name, category, auth, gender, start, finish, pass)
+	values('岸辺露伴', '書籍', 2, 'male', 8, 15, 'None');
 
 
