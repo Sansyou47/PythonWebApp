12a86d18eb4efddb3f16d66554f06cab8dac0c0a
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2023-10-24 09:32:35 +0900

    再利用

--- app/main.py
+++ app/main.py
@@ -1,21 +1,162 @@
-from flask import Flask
-from flask import render_template, request
+from flask import Flask, render_template, request, redirect
+from flaskext.mysql import MySQL
+from function import search
+import os, hashlib, pymysql
 
 app = Flask(__name__)
 
+# MySQL設定(環境変数から読み取り)
+app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE')
+app.config['MYSQL_DATABASE_HOST'] = 'mysql'
+
+mysql = MySQL(app)
+
+# indexルート
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/multiply', methods=['POST'])
-def multiply():
-    number = int(request.form['number'])
-    result = number * 2
-    return render_template('result.html', result=result)
+# 検索フォーム
+@app.route('/search')
+def search():
+    return render_template('/form/db_search.html')
 
-@app.route("/test")
+# 新規登録フォーム
+@app.route('/entry')
+def entry():
+    return render_template('/form/entry.html')
+
+# 休暇申請フォーム
+@app.route('/submit')
+def holiday():
+    return render_template('/form/holiday_submit.html')
+
+# ログインフォーム
+@app.route('/login')
+def login():
+    return render_template('/form/login.html')
+
+# 休暇申請状況確認フォーム
+@app.route('/regstatus')
+def regstatus():
+    return render_template("/form/regstatus.html")
+
+# 休暇申請状況確認処理
+@app.route('/action/regstatus', methods=['POST'])
+def do_regstatus():
+    id=int(request.form.get('number'))
+    cursor=mysql.get_db().cursor()
+    cursor.execute("SELECT id, date, type , status, regtime FROM holiday WHERE id=%s",(id))
+    data=cursor.fetchall()
+    cursor.close()
+    return render_template('/result/res_regstatus.html', data=data)
+
+# ログイン処理
+@app.route('/action/login', methods=['POST'])
+def do_login():
+    number = request.form['number']
+    password = request.form['pass']
+    # パスワードのハッシュ化
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    # データベースに接続
+    cursor = mysql.get_db().cursor()
+    # ユーザーの検索
+    cursor.execute("SELECT * FROM employee WHERE number=%s AND pass=%s", (number, hashed_password))
+    user = cursor.fetchone()
+    # 接続を閉じる
+    cursor.close()
+    if user is None:
+        return "ログインできませんでした。社員番号とパスワードを確認してください。"
+    return "ログイン成功！"
+
+# 希望休の登録処理
+@app.route('/action/submit', methods=['POST'])
+def submit():
+    id = request.form.get('id')
+    date = request.form['date']
+    holiday_type = request.form['holiday_type']
+    # データベースに接続
+    cursor = mysql.get_db().cursor()
+    # データを挿入
+    cursor.execute("INSERT INTO holiday (id, date, type) VALUES (%s, %s, %s)", (id, date, holiday_type))
+    # 変更を保存
+    mysql.get_db().commit()
+    # 接続を閉じる
+    cursor.close()
+    return render_template('/result/holiday_result.html')
+
+@app.route('/dbdelete')
+def dbdelte():
+    cur = mysql.get_db().cursor()
+    # テーブルの情報をすべて返す
+    cur.execute("SELECT * FROM employee")
+    data = cur.fetchall()
+    return render_template('/form/db_delete.html', data=data)
+
+# データの削除処理
+@app.route('/delete', methods=['POST'])
+def delete():
+    number=int(request.form.get('radio'))
+    # MySQLへ接続
+    conn=mysql.get_db()
+    cur=conn.cursor()
+    cur.execute("delete from employee where number = %s",(number))
+    conn.commit()
+    cur.close()
+    return redirect('/show')
+
+# 従業員番号から検索する処理
+@app.route('/numbersearch', methods=['POST'])
+def db_serch():
+    # POSTメソッドがリクエストされた場合のみ実行
+    if request.method == 'POST':
+        # フォームから値の受け取り
+        number=request.form.get('number')
+        if number == "None":
+            return render_template('index.html')
+        else:
+            cur = mysql.get_db().cursor()
+            # SQL実行
+            cur.execute("select name, category from employee where number = %s",(number))
+            data=cur.fetchone()
+            cur.close()
+            return render_template('/result/search_result.html', name=data[0],category=data[1])
+    # POSTメソッドがリクエストされていないのでリダイレクトする
+    else:
+        return redirect('/')
+
+# 新規登録フォームのデータをデータベースへ登録する処理
+@app.route('/dbinsert', methods=['POST'])
+def dbinsert():
+    # データの受け取り（5個）
+    name=request.form.get('name')
+    category=request.form.get('category')
+    auth=int(request.form.get('auth'))
+    gender=request.form.get('gender')
+    start=int(request.form.get('start'))
+    finish=int(request.form.get('finish'))
+    passwd=request.form.get('pass')
+    # パスワードはハッシュ化して登録する
+    passwdhs=hashlib.sha256(passwd.encode()).hexdigest()
+    # MySQLへ接続
+    conn=mysql.get_db()
+    cur=conn.cursor()
+    # SQL実行
+    cur.execute("INSERT INTO employee(name, category, auth, gender, start, finish, pass) VALUES(%s, %s, %s, %s, %s, %s, %s)",(name,category,auth,gender,start,finish,passwdhs))
+    conn.commit()
+    cur.close()
+    return redirect('/show')
+
+# employeeテーブルのデータを全取得
+@app.route('/show')
 def test():
-    return render_template('test.html')
+    cur = mysql.get_db().cursor()
+    # テーブルの情報をすべて返す
+    cur.execute("SELECT * FROM employee")
+    data = cur.fetchall()
+    return render_template('/result/show_dbdata.html', data=data)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
